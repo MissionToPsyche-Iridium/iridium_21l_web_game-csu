e518d8a52413f6d98d76280d6f753e65bc37bcde
TheMadSaucer <159360180+TheMadSaucer@users.noreply.github.com>
2024-11-25 20:48:04 -0500

    New assets folder with updated scripts
    Updated the camera rotational script to use quaternion rotation instead of direct transform rotation allowing for smooth clamping of the z-axis rotation.

--- Assets/RotateShip.cs
+++ Assets/RotateShip.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
 public class RotateShip : MonoBehaviour
-{
+{   //declare variables
     public Vector2 turn;
     public GameObject shipCenter;
     private float shipRotate = 0f;
+    private float rotateZ = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,26 +15,16 @@ public class RotateShip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 rotation = shipCenter.transform.localEulerAngles;
-        turn.x = Input.GetAxis("Mouse X");
-        turn.y = Input.GetAxis("Mouse Y");
-        //rotation.z += Mathf.Clamp(rotation.z, -20.0f, 20.0f);
-       /* if (shipCenter.transform.rotation.z < 0)
-        {
-            shipRotate = shipCenter.transform.rotation.eulerAngles.z;
-            shipRotate = Mathf.Clamp(shipRotate, 340.0f, 360.0f);
-        }
-        else
-        {
-            shipRotate = shipCenter.transform.rotation.eulerAngles.z;
-            shipRotate = Mathf.Clamp(shipRotate, 0.0f, 20.0f);
-        }
-       */
-        shipCenter.transform.Rotate(turn.y, turn.x, 0.0f);
-        //Mathf.Clamp(shipCenter.transform.rotation.eulerAngles.z, 20.0f, 340.0f);
-       /* Debug.Log(shipCenter.transform.rotation.z);
-        Debug.Log(shipCenter.transform.rotation.eulerAngles.z);
-        Debug.Log("custom" + shipRotate);
-       */
+        Vector3 rotation = shipCenter.transform.localEulerAngles;
+        //establish mouse input variables, and match horizontal mouse movement with ship's Z rotation
+        turn.x += Input.GetAxis("Mouse X");
+        rotateZ += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        //clamp z rotation on ship
+        rotateZ = Mathf.Clamp(rotateZ, -40f, 40f);
+
+        //rotate the ship
+        shipCenter.transform.rotation = Quaternion.Euler(turn.y, turn.x, -rotateZ);
+        
     }
 }
