2c901e91384847a1f34da89cd3bf7f1308569eee
Dylan Monreal <dylanmonreal1@gmail.com>
2025-02-01 18:09:35 -0500

    menu functionality

---
+++ Scripts/Menu/Options.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+     public void Volume()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
