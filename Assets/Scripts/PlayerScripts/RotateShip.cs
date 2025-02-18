ea70cb60929ad327ecc87294dd0823eceee468d9
TheMadSaucer <159360180+TheMadSaucer@users.noreply.github.com>
2025-02-17 21:01:04 -0500

    Forgot to update rotate ship script, polishing camera movement

--- Assets/Scripts/PlayerScripts/RotateShip.cs
+++ Assets/Scripts/PlayerScripts/RotateShip.cs
@@ -1,16 +1,22 @@
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotateShip : MonoBehaviour
 {   //declare variables
     public Vector2 turn;
     public GameObject shipCenter;
-    private float rotateZ = 0f;
-    public static bool flipY = false; //Dylan
-    public static float sensitivity = 1f; //Dylan 
+    private Rigidbody rb;
+
+
+    public InputAction RotateQ;
+    public InputAction RotateE;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RotateQ = InputSystem.actions.FindAction("RotateQ");
+        RotateE = InputSystem.actions.FindAction("RotateE");
+        rb = shipCenter.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,19 +24,22 @@ public class RotateShip : MonoBehaviour
     {
         Vector3 rotation = shipCenter.transform.localEulerAngles;
         //establish mouse input variables, and match horizontal mouse movement with ship's Z rotation
-        turn.x += Input.GetAxis("Mouse X") * (2*sensitivity);
-        //rotateZ += Input.GetAxis("Mouse X");
-        if (RotateShip.flipY == false) {
-            turn.y -= Input.GetAxis("Mouse Y") * (2*sensitivity);
+        turn.x = Input.GetAxis("Mouse X");
+       
+     
+        turn.y = Input.GetAxis("Mouse Y");
+    
+        if (RotateQ.IsPressed())
+        {
+            transform.Rotate(0f, 0f, .25f);
         }
-        else if (RotateShip.flipY == true) {
-            turn.y += Input.GetAxis("Mouse Y") * (2*sensitivity);
+        if (RotateE.IsPressed())
+        {
+            transform.Rotate(0f, 0f, -.25f);
         }
-        //clamp z rotation on ship
-        rotateZ = Mathf.Clamp(rotateZ, -40f, 40f);
+       
+        transform.Rotate(turn.y, turn.x, 0);
+      
 
-        //rotate the ship
-        shipCenter.transform.rotation = Quaternion.Euler(turn.y, turn.x, -rotateZ);
-        
     }
 }
