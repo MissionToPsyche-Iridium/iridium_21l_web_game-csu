48bf344487c5dc76bb524898a20da0df7d208370
Ruslan Averin <163885011+ErnakSalmander@users.noreply.github.com>
2025-02-25 15:53:27 -0500

    Add files via upload
    Contains the function for random asteroid sizes, need to improve

---
+++ SpawnAsteroid.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnAsteroid : MonoBehaviour
+{
+    private float random_numb;
+    public GameObject asteroid;
+    public Transform asteroidSpawnerTransform;
+    private GameObject newAsteroid;
+    private Rigidbody asteroidRB;
+    private float waitTime = 0f;
+    private float timer = 0.0f;
+    private float ranX, ranY, ranSpeed = 0.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        waitTime = Random.Range(3.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime)
+        {
+            timer -= waitTime;
+            ranX = Random.Range(-20f, 20f);
+            ranY = Random.Range(170f, 190f);
+            ranSpeed = Random.Range(3f, 15f);
+            asteroidSpawnerTransform.rotation = Quaternion.Euler(ranX, ranY, 0f);
+            spawnAsteroid();
+            waitTime = Random.Range(3.0f, 10.0f);
+        }
+    }
+
+    private void spawnAsteroid()
+    {
+        newAsteroid = Instantiate(asteroid, asteroidSpawnerTransform.position, asteroidSpawnerTransform.rotation);
+        asteroidRB = newAsteroid.GetComponent<Rigidbody>();
+
+        // Randomize the size of the asteroid by scaling it
+        float randomScale = Random.Range(0.5f, 10.0f); // Random scale between 0.5 and 3 times the original size
+        newAsteroid.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+
+        // Start scaling and deletion processes
+        StartCoroutine(ScaleAsteroid(asteroidRB, newAsteroid));
+        StartCoroutine(DeleteAsteroid(newAsteroid));
+
+        // Apply random velocity
+        asteroidRB.linearVelocity = asteroidRB.transform.TransformDirection(Vector3.forward * ranSpeed);
+    }
+
+    private IEnumerator ScaleAsteroid(Rigidbody asteroidRB, GameObject newAsteroid)
+    {
+        random_numb = Random.Range(7f, 50f);
+
+        for (int i = 0; i < random_numb; i++)
+        {
+            if (asteroidRB == null)
+            {
+                yield break; // Stop scaling if the asteroid is destroyed
+            }
+
+            newAsteroid.transform.localScale += new Vector3(.05f, .05f, .05f);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    private IEnumerator DeleteAsteroid(GameObject newAsteroid)
+    {
+        yield return new WaitForSeconds(5f);
+
+        if (newAsteroid != null)
+        {
+            Destroy(newAsteroid);
+        }
+    }
+}
